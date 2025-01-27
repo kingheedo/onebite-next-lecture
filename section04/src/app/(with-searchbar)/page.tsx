bdb0c1559b114891c55452c86242bed27cc58377
kingheedo <dhkdgmleh@gmail.com>
2025-01-27 17:59:48 +0900

    feat: 앱 라우터 데이터 페칭

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,38 @@
-import BookItem from "@/components/book-item";
-import style from "./page.module.css";
-import books from "@/mock/books.json";
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import { BookData } from '@/types';
+import { SERVER_URL } from '@/constants/server-url';
 
-export default function Home() {
+const AllBooks = async () => {
+  const response = await fetch(`${SERVER_URL}/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return allBooks.map((book) => <BookItem key={book.id} {...book} />);
+};
+
+const RecommendBooks = async () => {
+  const response = await fetch(`${SERVER_URL}/book/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const books: BookData[] = await response.json();
+
+  return books.map((book) => <BookItem key={book.id} {...book} />);
+};
+
+export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecommendBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- section04/src/app/(with-searchbar)/search/page.tsx
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,15 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { SERVER_URL } from '@/constants/server-url';
+import { BookData } from '@/types';
+
+const SearchBooks = async ({ query }: { query?: string }) => {
+  const response = await fetch(`${SERVER_URL}/book/search?q=${query}`);
+  if (!response.ok) {
+    return <div>도서 검색에 실패하였습니다...</div>;
+  }
+  const books: BookData[] = await response.json();
+  return books.map((book) => <BookItem key={book.id} {...book} />);
+};
 
 export default async function Page({
   searchParams,
@@ -11,10 +21,7 @@ export default async function Page({
   const { q } = await searchParams;
   return (
     <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-      {q}
+      <SearchBooks query={q} />
     </div>
   );
 }
